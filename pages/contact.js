5ce0b4c8e35d98fac7a00ccf16a5254dde5d2c47
wserhan <[wserhan@gmail.com]>
2022-05-26 06:49:00 -0400

    add form

--- pages/contact.js
+++ pages/contact.js
@@ -1,18 +1,16 @@
 import styles from '../styles/Contact.module.scss';
+import Form from '../components/Form';
 
 export default function Contact() {
   return (
     <section className={styles.container}>
       <h2>Contact</h2>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
-        optio accusamus dolorum error maxime commodi officiis rem! Ut
-        consequuntur, corporis at, aliquid architecto, saepe optio magni
-        perferendis delectus minus facere! Lorem ipsum dolor sit amet
-        consectetur adipisicing elit. Esse dolores tempora voluptatum nisi illum
-        cupiditate enim nulla praesentium, unde tenetur nostrum qui. Nobis
-        quaerat optio omnis sed iusto dicta aliquam.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita vel,
+        fuga libero esse, modi omnis magnam ex atque facere adipisci praesentium
+        tenetur rem, iste voluptatum at ipsa cumque labore vitae!
       </p>
+      <Form />
     </section>
   );
 }
